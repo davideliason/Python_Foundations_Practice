f336712ed53bf42da02329d94633de09d138185c
David Eliason <davethemaker@gmail.com>
2022-04-22 06:00:06 -0700

    create project for reading CSV data and create a report using some filters

---
+++ projects/Reading_CSV_data_for_report.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import csv
+
+# read_employees will receive CSV file as a parameter
+# return a list of dictionaries from that file
+
+def read_employees(csv_file_location):
+# file_location is the path to employees.csv as a parameter
